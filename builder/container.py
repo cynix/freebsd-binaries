98f143a8437f0ceb6446b2fb868e6cfc9a3e0225
cynix <cynix@cynix.org>
2025-10-22 20:46:22 +1100

    Clean up config schema

--- builder/container.py
+++ builder/container.py
@@ -1,20 +1,25 @@
 import os
-import re
 import shutil
 import subprocess
-import sys
 import tarfile
 from contextlib import contextmanager
-from fnmatch import fnmatchcase
 from pathlib import Path
 from textwrap import dedent
 
 import requests
 from actions import core
 from actions.github import get_githubkit
-from ruamel.yaml import YAML
 
-from .utils import get_release
+from .config import (
+    ArchiveFile,
+    CargoProject,
+    Config,
+    Container,
+    ContainerProject,
+    FileAsset,
+    GoProject,
+    PkgAsset,
+)
 
 
 def buildah(*args: str, text: bool = True) -> str:
@@ -57,12 +62,11 @@ def pkg(
             file=f,
         )
 
-    env = dict(os.environ)
-    env.update(
-        IGNORE_OSVERSION="yes",
-        PKG_CACHEDIR="/tmp/cache",
-        ABI=f"FreeBSD:{major}:{'aarch64' if arch == 'arm64' else arch}",
-    )
+    env = os.environ | {
+        "IGNORE_OSVERSION": "yes",
+        "PKG_CACHEDIR": "/tmp/cache",
+        "ABI": f"FreeBSD:{major}:{'aarch64' if arch == 'arm64' else arch}",
+    }
 
     if text:
         return subprocess.check_output(
@@ -71,23 +75,6 @@ def pkg(
     else:
         subprocess.check_call(["pkg", "--rootdir", m, cmd, "-y"] + list(args), env=env)
         return ""
-
-
-def get_version(version: str | dict[str, str] | None) -> str | None:
-    if not version:
-        return None
-
-    if isinstance(version, str):
-        return version
-
-    body = requests.get(version["url"]).text
-    if "regex" not in version:
-        return body
-
-    if m := re.search(version["regex"], body):
-        return m.group("version")
-
-    return None
 
 
 @contextmanager
@@ -107,10 +94,9 @@ def container(manifest: str, base: str, arch: str):
             buildah("rm", c)
 
 
-def calculate_dst(src: str, dst: str) -> str:
+def _calculate_dst(src: str, dst: str) -> str:
     if not dst.startswith("/"):
-        core.set_failed(f"Invalid dst: {dst}")
-        sys.exit(1)
+        raise ValueError(f"Invalid dst: {dst}")
 
     if dst.endswith("/"):
         dst = f"{dst}{src.rsplit('/', 1)[-1]}"
@@ -118,7 +104,7 @@ def calculate_dst(src: str, dst: str) -> str:
     return dst
 
 
-def extract_tarball(m: Path, url: str, files: list[dict[str, str]]) -> str | None:
+def _extract_archive(m: Path, url: str, files: list[ArchiveFile]) -> str | None:
     t = m / "tmp/tarball"
     t.mkdir()
 
@@ -133,20 +119,20 @@ def extract_tarball(m: Path, url: str, files: list[dict[str, str]]) -> str | Non
     entrypoint = None
 
     for file in files:
-        src = file["src"]
-        dst = file.get("dst", "/usr/local/bin/")
-
-        if not dst.startswith("/"):
-            raise RuntimeError(f"Invalid dst: {dst}")
+        src = str(file.src)
+        dst = str(file.dst)
 
         dir = src.endswith("/")
         src = src.rstrip("/")
 
         for s in t.glob(src):
             if s.is_dir() != dir:
+                core.info(
+                    f"{s.relative_to(t)} is a {'dir' if s.is_dir() else 'file'} but we want a {'dir' if dir else 'file'}"
+                )
                 continue
 
-            d = calculate_dst(s.name, dst)
+            d = _calculate_dst(s.name, dst)
 
             if not entrypoint and s.is_file() and s.stat().st_mode & 0o111 == 0o111:
                 entrypoint = d
@@ -159,65 +145,55 @@ def extract_tarball(m: Path, url: str, files: list[dict[str, str]]) -> str | Non
             s.replace(d)
             break
         else:
-            core.set_failed(f"{src} not found")
-            sys.exit(1)
+            raise ValueError(f"{src} not found")
 
     return entrypoint
 
 
-def build_container():
+def _build_container():
     gh = get_githubkit()
 
     project = core.get_input("project")
+    version = core.get_input("version")
     name = core.get_input("container")
 
-    with open("projects.yaml") as f:
-        y = YAML().load(f)[project]
+    config = Config.from_yaml("projects.yaml").root[project]
+    archs = config.arch
 
-    if config := y.get("container"):
-        pass
-    elif go := y.get("go"):
-        go = go["packages"][name]
-        config = go["container"]
-        binaries = go.get("binaries", [name])
-        config["assets"] = [
-            {
-                "release": "cynix/freebsd-binaries",
-                "match": f"/{re.escape(project)}-v(?P<version>{re.escape(core.get_input('version'))})/",
-                "glob": f"{name}-*-freebsd_{{arch}}.tar.gz",
-                "files": [{"src": f"**/{x}"} for x in binaries]
-                + config.pop("files", []),
-            }
-        ]
+    if isinstance(config, (CargoProject, GoProject)):
+        assert project == name
+        config = config.packages[name].container
+    elif isinstance(config, ContainerProject):
+        config = config.container
     else:
-        core.set_failed("Unknown project type")
-        return 1
+        raise TypeError("Unknown project type")
+
+    assert isinstance(config, Container)
 
     latest = f"ghcr.io/cynix/{name}:latest"
-    tagged = ""
+    tagged = f"ghcr.io/cynix/{name}:{version}" if version else ""
     buildah("manifest", "create", latest)
 
-    base = config.get(
-        "base",
+    base = config.base or (
         "freebsd:runtime"
-        if any("pkg" in x for x in config["assets"])
+        if any(isinstance(x, PkgAsset) for x in config.assets)
         else "freebsd:static",
     )
+    base = f"ghcr.io/cynix/{base}"
 
-    image = f"ghcr.io/cynix/{base}"
-    subprocess.check_call(["podman", "pull", image])
-    version = subprocess.check_output(
+    subprocess.check_call(["podman", "pull", base])
+    freebsd_version = subprocess.check_output(
         [
             "podman",
             "image",
             "inspect",
             '--format={{index .Annotations "org.freebsd.version"}}',
-            image,
+            base,
         ],
         text=True,
     ).strip()
 
-    for arch in y.get("arch", ["amd64", "arm64"]):
+    for arch in archs:
         core.info(f"Building arch: {arch}")
 
         triple = f"{arch.replace('amd64', 'x86_64').replace('arm64', 'aarch64')}-unknown-freebsd"
@@ -228,9 +204,9 @@ def build_container():
                 core.info(f"Copying {root}")
                 shutil.copytree(root, m, symlinks=True, dirs_exist_ok=True)
 
-            if user := config.get("user"):
+            if user := config.user:
                 if "=" in user:
-                    user, uid = user.split("=")
+                    user, uid = user.rsplit("=", 1)
                     core.info(f"Creating {user} = {uid}")
 
                     pw(m, "groupadd", "-n", user, "-g", uid)
@@ -249,23 +225,41 @@ def build_container():
                         "/sbin/nologin",
                     )
 
-            versions = {}
+            pkg_versions = {}
 
             # Install all packages in one go for efficiency
-            if pkgs := [x["pkg"] for x in config["assets"] if "pkg" in x]:
+            if pkgs := [x.pkg for x in config.assets if isinstance(x, PkgAsset)]:
                 core.info(f"Installing packages: {' '.join(pkgs)}")
-                pkg(version, arch, m, "install", *pkgs)
+                pkg(freebsd_version, arch, m, "install", *pkgs)
+                pkg_versions = dict(
+                    zip(
+                        pkgs,
+                        (
+                            x.strip()
+                            for x in pkg(
+                                freebsd_version,
+                                arch,
+                                m,
+                                "query",
+                                "%v",
+                                *pkgs,
+                                text=True,
+                            ).splitlines()
+                        ),
+                    )
+                )
 
-                versions = {
-                    x: pkg(version, arch, m, "query", "%v", x, text=True) for x in pkgs
-                }
                 shutil.rmtree(m / "var/db/pkg/repos")
 
                 hints = set(["/lib", "/usr/lib", "/usr/local/lib"])
 
                 for conf in (m / "usr/local/libdata/ldconfig").glob("*"):
                     with open(conf) as f:
-                        hints.update(x for x in f.read().splitlines() if x)
+                        hints.update(
+                            x.strip()
+                            for x in f.read().splitlines()
+                            if x and not x.startswith("#")
+                        )
 
                 # Ensure dirs exist before running `ldconfig` on the host
                 for d in hints:
@@ -275,30 +269,18 @@ def build_container():
                     ["ldconfig", "-f", m / "var/run/ld-elf.so.hints"] + sorted(hints)
                 )
 
-            for asset in config["assets"]:
-                if p := asset.get("pkg"):
-                    buildah(
-                        "config",
-                        f"--annotation=org.freebsd.pkg.{p}.version={versions[p]}",
-                        c,
+            for asset in config.assets:
+                if isinstance(asset, FileAsset):
+                    url, ver = asset.resolve(
+                        project=project,
+                        version=version,
+                        package=name,
+                        arch=arch,
+                        triple=triple,
                     )
+                    dst = _calculate_dst(url, asset.dst or f"/usr/local/{name}/")
 
-                    if not tagged:
-                        core.info(
-                            f"Deduced image version from package {p}: {versions[p]}"
-                        )
-                        tagged = f"ghcr.io/cynix/{name}:{versions[p]}"
-
-                    if "entrypoint" not in config:
-                        core.info(f"Deduced entrypoint from package {p}")
-                        config["entrypoint"] = f"/usr/local/bin/{p}"
-
-                elif url := asset.get("file"):
-                    ver = get_version(asset.get("version"))
-                    url = url.format(version=ver, arch=arch, triple=triple)
-                    dst = calculate_dst(url, asset.get("dst", f"/usr/local/{name}/"))
-
-                    core.info(f"Downloading {url}")
+                    core.info(f"{url} -> {dst}")
 
                     with requests.get(url, stream=True) as r:
                         r.raise_for_status()
@@ -310,63 +292,70 @@ def build_container():
                             shutil.copyfileobj(r.raw, f)
 
                         if ver and not tagged:
-                            core.info(f"Deduced image version: {ver}")
+                            core.info(f"Deduced image version from file: {url} = {ver}")
                             tagged = f"ghcr.io/cynix/{name}:{ver}"
 
-                        if "entrypoint" not in config:
+                        if not config.entrypoint:
                             core.info(f"Deduced entrypoint: {dst}")
                             os.chmod(out, 0o755)
-                            config["entrypoint"] = dst
+                            config.entrypoint = dst
+                elif isinstance(asset, PkgAsset):
+                    ver = pkg_versions[asset.pkg]
+                    buildah(
+                        "config",
+                        f"--annotation=org.freebsd.pkg.{asset.pkg}.version={ver}",
+                        c,
+                    )
 
+                    if not tagged:
+                        core.info(
+                            f"Deduced image version from package: {asset.pkg} = {ver}"
+                        )
+                        tagged = f"ghcr.io/cynix/{name}:{ver}"
+
+                    if not config.entrypoint:
+                        core.info(
+                            f"Deduced entrypoint from package: /usr/local/bin/{asset.pkg}"
+                        )
+                        config.entrypoint = f"/usr/local/bin/{asset.pkg}"
                 else:
-                    url, ver = None, None
-
-                    if repo := asset.get("release"):
-                        core.info(f"Fetching {repo} release")
-
-                        rls, ver = get_release(gh, repo, asset.get("match"))
-                        glob = asset["glob"].format(arch=arch, triple=triple)
-
-                        for ra in rls.assets:
-                            if fnmatchcase(
-                                ra.browser_download_url.rsplit("/", 1)[-1], glob
-                            ):
-                                url = ra.browser_download_url
-                                break
-                        else:
-                            core.set_failed(f"{glob} not found in {rls.name}")
-                            return 1
-                    elif url := asset.get("tarball"):
-                        ver = get_version(asset.get("version"))
-                        url = url.format(version=ver, arch=arch, triple=triple)
-                    else:
-                        core.set_failed(f"Unknown asset type: {asset}")
-                        return 1
-
-                    files = asset.get("files", [{"src": f"**/{name}"}])
-                    entrypoint = extract_tarball(m, url, files)
+                    url, ver = asset.resolve(
+                        gh=gh,
+                        project=project,
+                        version=version,
+                        package=name,
+                        arch=arch,
+                        triple=triple,
+                    )
+                    entrypoint = _extract_archive(
+                        m, url, asset.files or [ArchiveFile(src=f"**/{name}")]
+                    )
 
                     if ver and not tagged:
                         tagged = f"ghcr.io/cynix/{name}:{ver}"
 
-                    if entrypoint and "entrypoint" not in config:
-                        config["entrypoint"] = entrypoint
+                    if not config.entrypoint:
+                        core.info(
+                            f"Deduced entrypoint from archive: {url} = {entrypoint}"
+                        )
+                        config.entrypoint = entrypoint
 
             if (m / "usr/local/sbin").is_dir():
                 os.chmod(m / "usr/local/sbin", 0o711)
 
-            if script := config.get("script"):
+            if config.script:
                 core.info("Running build script")
                 subprocess.run(
-                    ["sh", "-ex"], cwd=m, input=script, text=True, check=True
+                    ["sh", "-ex"], cwd=m, input=config.script, text=True, check=True
                 )
 
-            if isinstance(config["entrypoint"], str):
-                config["entrypoint"] = [config["entrypoint"]]
+            if isinstance(config.entrypoint, str):
+                config.entrypoint = [config.entrypoint]
+            assert isinstance(config.entrypoint, list)
+            entrypoint = ",".join(f'"{x}"' for x in config.entrypoint)
 
-            entrypoint = ",".join(f'"{x}"' for x in config["entrypoint"])
             cmd = ["config", f"--entrypoint=[{entrypoint}]", "--cmd="] + [
-                f"--env={k}={v}" for k, v in config.get("env", {}).items()
+                f"--env={k}={v}" for k, v in config.env.items()
             ]
 
             if user:
@@ -378,3 +367,11 @@ def build_container():
 
     if tagged:
         buildah("manifest", "push", "--all", latest, f"docker://{tagged}")
+
+
+def build_container():
+    try:
+        _build_container()
+    except Exception as e:
+        core.set_failed(e)
+        return 1
